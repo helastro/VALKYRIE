4cd17898b91500291f67843dbcf0941f1f90a3b6
helastro <helmyfirdy888@gmail.com>
2023-05-23 21:14:42 +0700

    masuk

---
+++ main/masuk.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("masuk");
+var y = document.getElementById("daftar");
+var z = document.getElementById("btn");
+
+function daftar() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+}
+
+function masuk() {
+  x.style.left = "50px";
+  y.style.left = "440px";
+  z.style.left = "0";
+}
